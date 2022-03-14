964ba9ead66f9ecc93b608e46fab271d7674ad7d
Rethabile Lemao <99502602+Lemao07@users.noreply.github.com>
2022-03-14 14:55:53 +0200

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,77 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *   * main - Prints 3 combination of numbers
- *
- *     *
- *
- *       * Return: Always (Success)
- *
- *         */
-
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
-
 {
-<<<<<<< HEAD
-
-		int c, i, k;
-
-
-
-			for (c = '0'; c <= '9'; c++)
-
-					{
-
-								for (i = '0'; i <= '9'; i++)
-
-											{
-
-															for (k = '0'; k <= '9'; k++)
-
-																			{
-
-																								if (c < i && i < k)
-
-																													{
-
-																																			putchar(c);
-
-																																								putchar(i);
-
-																																													putchar(k);
-
-
-
-																																																		if (c != '7')
-
-																																																								{
-
-																																																															putchar(',');
-
-																																																																					putchar(' ');
-
-																																																																										}
-
-																																																						}
-
-																											}
-
-																	}
-
-									}
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
-=======
 	int c, i, k;
 
 	for (c = '0'; c <= '9'; c++)
@@ -99,7 +34,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
->>>>>>> 7ae77597b84020e1bbe92a85920caf9f063d2028
 }
-
-
