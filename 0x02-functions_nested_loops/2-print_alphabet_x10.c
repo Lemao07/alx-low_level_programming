e6d161f47ecb22b764627fb1037e50b4489d2830
Rethabile Lemao <99502602+Lemao07@users.noreply.github.com>
2022-03-16 20:44:28 +0200

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,47 +1,24 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_alphabet_x10 - prints alphabets 10 times
- *
- *   * Return:void
- *
- *    */
-
-
+ * print_alphabet_x10 - prints alphabets 10 times
+ * Return:void
+ */
 
 void print_alphabet_x10(void)
-
 {
+	char c;
+	int i = 0;
 
-		char c;
-
-			int i = 0;
-
-
-
-				while (i < 10)
-
-						{
-
-									c = 'a';
-
-											while (c <= 'z')
-
-														{
-
-																		_putchar(c);
-
-																					c++;
-
-																							}
-
-													_putchar('\n');
-
-																i++;
-
-																	}
-
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+			i++;
+	}
 }
