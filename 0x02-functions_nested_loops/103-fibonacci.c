e6441a3806cb3e8e094024b9ddc838c606a02aee
Rethabile Lemao <99502602+Lemao07@users.noreply.github.com>
2022-03-16 21:42:13 +0200

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,55 +1,28 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *   * main - Prints the sum of Even Fibonacci numbers
- *
- *     * less than 4000000.
- *
- *       *
- *
- *         * Return: Nothing!
- *
- *           */
-
+  * main - Prints the sum of Even Fibonacci numbers
+  * less than 4000000.
+  *
+  * Return: Nothing!
+  */
 int main(void)
-
 {
+	int i = 0;
+	long j = 1, k = 2, sum = k;
 
-		int i = 0;
+	while (k + j < 4000000)
+	{
+		k += j;
 
-			long j = 1, k = 2, sum = k;
+		if (k % 2 == 0)
+			sum += k;
 
+		j = k - j;
 
+		++i;
+	}
 
-				while (k + j < 4000000)
-
-						{
-
-									k += j;
-
-
-
-											if (k % 2 == 0)
-
-															sum += k;
-
-
-
-													j = k - j;
-
-
-
-															++i;
-
-																}
-
-
-
-					printf("%ld\n", sum);
-
-						return (0);
-
+	printf("%ld\n", sum);
+	return (0);
 }
