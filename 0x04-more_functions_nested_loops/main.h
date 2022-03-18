9e3727ad24bd5e9c7b1511595de6c2ffdf26b3b6
Rethabile Lemao <99502602+Lemao07@users.noreply.github.com>
2022-03-18 15:24:52 +0200

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,35 +1,17 @@
-#ifndef _main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-#define _main_h
-
-
-
+int _isupper(int c);
 int _putchar(char c);
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int n);
-
-int print_sign(int n);
-
-int _abs(int n);
-
-int print_last_digit(int n);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int one, int two);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-
+int _isdigit(int c);
+int mul(int a, int b);
+void print_most_numbers(void);
+void print_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
