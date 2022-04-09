4fc2cb11aa5ee4b8fea3fd157f2c976af99436ca
Lemao07 <glendaharbee@gmail.com>
2022-04-08 18:48:57 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <limits.h>
+
+
+
+/**
+ *
+ *  * malloc_checked - Entry point
+ *
+ *   * @b: input amount
+ *
+ *    * Return: pointer to a new memory
+ *
+ *     */
+
+void *malloc_checked(unsigned int b)
+
+{
+
+		void *n =  malloc(b);
+
+
+
+			if (n == NULL)
+
+					
+
+						exit(98);
+
+
+
+				return (n);
+
+}
